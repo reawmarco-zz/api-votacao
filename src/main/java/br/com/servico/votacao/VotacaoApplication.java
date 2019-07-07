d5aa1601b582bc1d77389c9ee979a807ffd498bc
marco <marco.141992@gmail.com>
2019-07-07 18:26:00 -0300

    #GCP Configuracao para AppEngine Alterado pom.xml Adicionado dependencias para o AppEngine

--- src/main/java/br/com/servico/votacao/VotacaoApplication.java
+++ src/main/java/br/com/servico/votacao/VotacaoApplication.java
@@ -2,14 +2,21 @@ package br.com.servico.votacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class VotacaoApplication {
+public class VotacaoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(VotacaoApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(VotacaoApplication.class);
     }
 
 }
